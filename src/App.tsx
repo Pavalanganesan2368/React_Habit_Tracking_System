660a38367958b75be0595bd767b17e58298b69f1
Pavalanganesan2368 <pavalanganesan2368@gmail.com>
2026-05-06 15:35:53 +0530

    Project Completed

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ function App() {
     start : startOfWeek(week, { weekStartsOn : 1 }), 
     end : endOfWeek(week, { weekStartsOn : 1 })
   });
+
+  
   return (
     <>
       <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
